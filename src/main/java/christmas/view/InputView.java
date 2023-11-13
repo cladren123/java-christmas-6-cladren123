f88e5416d04a929ca4f1490dc33fdd6145e1ba58
KimDongEon <gold3987@naver.com>
2023-11-13 18:28:32 +0900

    feat(InputView) : 날짜 입력 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,6 +1,7 @@
 package christmas.view;
 
 
+import camp.nextstep.edu.missionutils.Console;
 import christmas.constant.message.InputMessage;
 
 /**
@@ -11,6 +12,12 @@ public class InputView {
   // 환영 인사
   public void welcome() {
     System.out.println(InputMessage.WELCOME);
+  }
+
+  // 날짜 입력
+  public String readDate() {
+    System.out.println(InputMessage.READ_DATE);
+    String InputDate = Console.readLine();
   }
 
 }
