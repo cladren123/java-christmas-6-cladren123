ed55231572aa54cfcd3526498c9e0073a5e6e369
KimDongEon <gold3987@naver.com>
2023-11-13 18:27:26 +0900

    feat(InputView) : 환영 인사 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,16 @@
+package christmas.view;
+
+
+import christmas.constant.message.InputMessage;
+
+/**
+ * 입력 화면 출력
+ */
+public class InputView {
+
+  // 환영 인사
+  public void welcome() {
+    System.out.println(InputMessage.WELCOME);
+  }
+
+}
