a80cc57c71361ff5d781de1ae4ac0d1dec4f8e97
KimDongEon <gold3987@naver.com>
2023-11-13 23:45:58 +0900

    fix(InputView) : enum 상수에 .getMessage() 추가

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -11,19 +11,19 @@ public class InputView {
 
   // 환영 인사
   public void welcome() {
-    System.out.println(InputMessage.WELCOME);
+    System.out.println(InputMessage.WELCOME.getMessage());
   }
 
   // 날짜 입력
   public String readDate() {
-    System.out.println(InputMessage.READ_DATE);
+    System.out.println(InputMessage.READ_DATE.getMessage());
     String inputDate = Console.readLine();
     return inputDate;
   }
 
   // 메뉴 입력
   public String readMenu() {
-    System.out.println(InputMessage.READ_MENU);
+    System.out.println(InputMessage.READ_MENU.getMessage());
     String inputMenu = Console.readLine();
     return inputMenu;
   }
