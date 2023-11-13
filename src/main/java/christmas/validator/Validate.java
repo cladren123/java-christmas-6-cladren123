dc40eef47fbec83534836643c99798c92579cbe8
KimDongEon <gold3987@naver.com>
2023-11-13 21:01:45 +0900

    feat(Validate) : 메뉴 존재 검증 구현 - validateMenu()

--- src/main/java/christmas/validator/Validate.java
+++ src/main/java/christmas/validator/Validate.java
@@ -1,6 +1,13 @@
 package christmas.validator;
 
 
+import christmas.constant.menu.Menu;
+import christmas.constant.menu.MenuCategory;
+import jdk.jfr.Category;
+
+import java.io.IOException;
+import java.util.Map;
+
 /**
  * 검증 작업을 수행하는 클래스
  */
@@ -21,6 +28,19 @@ public class Validate {
       throw new IllegalArgumentException();
     }
   }
+
+  // 메뉴가 있는지 검증
+  public Menu validateMenu(String name) {
+    for(Menu menu : Menu.values()) {
+      if (menu.getName().equals(name)) {
+        return menu;
+      }
+    }
+    throw new IllegalArgumentException();
+  }
+
+
+
 
 
 
