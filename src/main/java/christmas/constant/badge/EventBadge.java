7a28579c26d75f778f8c2b76abd7f3e26e368e79
KimDongEon <gold3987@naver.com>
2023-11-13 17:22:52 +0900

    feat(EventBadge) : 이벤트 뱃지 상수 enum 구현

---
+++ src/main/java/christmas/constant/badge/EventBadge.java
@@ -0,0 +1,23 @@
+package christmas.constant.badge;
+
+public enum EventBadge {
+  STAR("별", 5000),
+  TREE("트리", 10000),
+  SANTA("산타", 20000);
+
+  private String name;
+  private int price;
+
+  EventBadge(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getPrice() {
+    return price;
+  }
+}
