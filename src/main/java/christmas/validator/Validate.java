2b82002ffa57ee50e5a10ea784bf408e1b582fea
KimDongEon <gold3987@naver.com>
2023-11-14 02:22:51 +0900

    fix(Validate) : 모든 메소드에 static으로 변경

--- src/main/java/christmas/validator/Validate.java
+++ src/main/java/christmas/validator/Validate.java
@@ -15,7 +15,7 @@ public class Validate {
 
 
   // 숫자인지 검증
-  public void validateNumber(String input) {
+  public static void validateNumber(String input) {
     try {
       Integer.parseInt(input);
     } catch (NumberFormatException e) {
@@ -24,17 +24,14 @@ public class Validate {
   }
 
   // 날짜의 범위 검증
-  public void validateDateNumberRange(int date) {
+  public static void validateDateNumberRange(int date) {
     if(date < 1 || date > 31) {
       throw new IllegalArgumentException();
     }
   }
 
-
-
-
   // 메뉴가 있는지 검증
-  public Menu validateMenu(String name) {
+  public static Menu validateMenu(String name) {
     for(Menu menu : Menu.values()) {
       if (menu.getName().equals(name)) {
         return menu;
@@ -44,35 +41,35 @@ public class Validate {
   }
 
   // 메뉴 개수가 1이상 20이하인지 확인
-  public void validateMenuNumberRange(int number) {
+  public static void validateMenuNumberRange(int number) {
     if(number < 1 || number > 20) {
       throw new IllegalArgumentException();
     }
   }
 
   // 메뉴가 중복인지 검증
-  public void validateMenuDuplicated(Map<String, Integer> order, String menuName) {
+  public static void validateMenuDuplicated(Map<String, Integer> order, String menuName) {
     if (order.containsKey(menuName)) {
       throw new IllegalArgumentException();
     }
   }
 
   // 메뉴의 개수가 없는 경우 검증
-  public void validateMenuHaveNumber(String[] data) {
+  public static void validateMenuHaveNumber(String[] data) {
     if(data.length != 2) {
       throw new IllegalArgumentException();
     }
   }
 
   // 음료만 있는지 확인
-  public void validateMenuOnlyBeverage(Map<String, Integer> orderCategory) {
+  public static void validateMenuOnlyBeverage(Map<String, Integer> orderCategory) {
     if (orderCategory.size() == 1 && orderCategory.containsKey("beverage")) {
       throw new IllegalArgumentException();
     }
   }
 
   // 메뉴 개수가 20개 이하인지 검증
-  public void validateMenuMaximum(Map<String, Integer> order) {
+  public static void validateMenuMaximum(Map<String, Integer> order) {
     int count = 0;
     for (String menu : order.keySet()) {
       count += order.get(menu);
