34908096780d58d1727463cf4f685320f73ffb2c
KimDongEon <gold3987@naver.com>
2023-11-13 18:52:20 +0900

    feat(Validate) : 검증 작업을 수행하는 Validate 클래스 생성

--- src/main/java/christmas/controller/EventPlanner.java
+++ src/main/java/christmas/controller/EventPlanner.java
@@ -1,12 +1,30 @@
 package christmas.controller;
 
+import christmas.view.InputView;
+
 /**
  * 전체적인 흐름을 설정합니다.
  */
 public class EventPlanner {
 
+  private final InputView inputView;
+
+  // 생성자 주입
+  EventPlanner(InputView inputView) {
+    this.inputView = inputView;
+  }
+
   public void start() {
 
+
+  }
+
+  // 날짜 입력
+  public int readDate() {
+    // todo : 검증 클래스와 올바른 입력이 올 때까지 반복 구현
+    String inputDate = inputView.readDate();
+    int date = Integer.parseInt(inputDate);
+    return date;
   }
 
 }
---
+++ src/main/java/christmas/validator/Validate.java
@@ -0,0 +1,13 @@
+package christmas.validator;
+
+
+/**
+ * 검증 작업을 수행하는 클래스
+ */
+public class Validate {
+
+
+
+
+
+}
