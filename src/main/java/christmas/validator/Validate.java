d830ceb9bbf5bdcdfaa8138c950df5c064942744
KimDongEon <gold3987@naver.com>
2023-11-13 21:52:17 +0900

    fix(Validate) : validateNumber의 반환값을 int로 변경

--- src/main/java/christmas/validator/Validate.java
+++ src/main/java/christmas/validator/Validate.java
@@ -14,9 +14,9 @@ import java.util.Map;
 public class Validate {
 
   // 숫자인지 검증
-  public void validateNumber(String input) {
+  public int validateNumber(String input) {
     try {
-      Integer.parseInt(input);
+      return Integer.parseInt(input);
     } catch (NumberFormatException e) {
       throw new IllegalArgumentException();
     }
