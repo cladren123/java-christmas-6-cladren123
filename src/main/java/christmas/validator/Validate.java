0444f6b091a3069c2a1579801b234a23d5cb08a6
KimDongEon <gold3987@naver.com>
2023-11-13 19:47:20 +0900

    feat(Validate) : 숫자인지 검증하는 로직 구현 - validateNumber()

--- src/main/java/christmas/validator/Validate.java
+++ src/main/java/christmas/validator/Validate.java
@@ -6,6 +6,15 @@ package christmas.validator;
  */
 public class Validate {
 
+  // 숫자인지 검증
+  public void validateNumber(String input) {
+    try {
+      Integer.parseInt(input);
+    } catch (NumberFormatException e) {
+      throw new IllegalArgumentException();
+    }
+  }
+
 
 
 
