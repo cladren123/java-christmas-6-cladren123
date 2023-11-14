4553df162f04fe1cf57bca5ace2311d2e254ed58
KimDongEon <gold3987@naver.com>
2023-11-14 18:45:16 +0900

    feat(EventBadge) : NOTHING 추가

--- src/main/java/christmas/constant/badge/EventBadge.java
+++ src/main/java/christmas/constant/badge/EventBadge.java
@@ -1,9 +1,11 @@
 package christmas.constant.badge;
 
 public enum EventBadge {
+  NOTHING("없음", 0),
   STAR("별", 5000),
   TREE("트리", 10000),
   SANTA("산타", 20000);
+
 
   private String name;
   private int price;
