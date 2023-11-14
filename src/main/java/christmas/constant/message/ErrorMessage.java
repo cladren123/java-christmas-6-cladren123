749c560fc05e4fdf76313ff0dd4f2a867d3a8948
KimDongEon <gold3987@naver.com>
2023-11-15 01:31:36 +0900

    fix(ErrorMessage) : 오타 수정

--- src/main/java/christmas/constant/message/ErrorMessage.java
+++ src/main/java/christmas/constant/message/ErrorMessage.java
@@ -2,7 +2,7 @@ package christmas.constant.message;
 
 public enum ErrorMessage {
 
-  INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요"),
+  INVALID_DATE("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요."),
   INVALID_MENU("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
 
   private String message;
