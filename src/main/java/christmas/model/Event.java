7672697f37894dec05aaab60dc37692874c9fca2
KimDongEon <gold3987@naver.com>
2023-11-14 15:39:58 +0900

    feat(Event) : 이벤트 파일 생성

---
+++ src/main/java/christmas/model/Event.java
@@ -0,0 +1,20 @@
+package christmas.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 이벤트 로직 처리 (할인, 증정, 이벤트 배지)
+ */
+public class Event {
+
+  private final Order order;
+
+  public Event(Order order) {
+    this.order = order;
+  }
+
+
+
+
+}
