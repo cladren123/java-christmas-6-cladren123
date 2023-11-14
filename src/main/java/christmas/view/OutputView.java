888b5f51fd5d50b7cbef48191f70daa97cb2d363
KimDongEon <gold3987@naver.com>
2023-11-14 22:42:16 +0900

    feat(OutputView) : printMenu 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -3,6 +3,8 @@ package christmas.view;
 
 import christmas.constant.message.OutputMessage;
 
+import java.util.Map;
+
 /**
  * 출력 화면 생성
  */
@@ -10,7 +12,15 @@ public class OutputView {
 
   // 인트로 문구 출력
   public void printIntro() {
-    System.out.println(OutputMessage.INTRO);
+    System.out.println(OutputMessage.INTRO.getMessage());
+  }
+
+  // 주문한 메뉴와 개수 출력
+  public void printMenu(Map<String, Integer> orderMenu) {
+    System.out.println(OutputMessage.ORDER_MENU.getMessage());
+    for (String name : orderMenu.keySet()) {
+      System.out.println(String.format(OutputMessage.ORDER_MENU_FORMAT.getMessage(), name, orderMenu.get(name)));
+    }
   }
 
 
