d11688963e3c38049f0e1c51a24dcf5e9ae71b98
KimDongEon <gold3987@naver.com>
2023-11-14 23:55:39 +0900

    feat(OutputMessage) : format 추가 및 \n 제거

--- src/main/java/christmas/constant/message/OutputMessage.java
+++ src/main/java/christmas/constant/message/OutputMessage.java
@@ -5,17 +5,21 @@ package christmas.constant.message;
  */
 public enum OutputMessage {
 
-  INTRO("에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
-  NOTHING("없음\n"),
-  ORDER_MENU("<주문 메뉴>\n"),
+  INTRO("에 우테코 식당에서 받을 이벤트 혜택 미리 보기!"),
+  NOTHING("없음"),
+  ORDER_MENU("<주문 메뉴>"),
   ORDER_MENU_FORMAT("%s %d개"),
-  ORDER_MONEY("<할인 전 총주문 금액>\n"),
-  GIVEAWAY_MENU("<증정 메뉴>\n"),
-  GIVEAWAY_MENU_TRUE("샴페인 1개\n"),
-  BENEFIT("<혜택 내역>\n"),
-  BENEFIT_MONEY("<총혜택 금액>\n"),
-  ORDER_MONEY_AFTER_DISCOUNT("<할인 후 예상 결제 금액>\n"),
-  EVENT_BADGE("<12월 이벤트 배지>\n");
+  ORDER_MONEY("<할인 전 총주문 금액>"),
+  ORDER_MONEY_FORMAT("%,d원"),
+  GIVEAWAY_MENU("<증정 메뉴>"),
+  GIVEAWAY_MENU_TRUE("샴페인 1개"),
+  BENEFIT("<혜택 내역>"),
+  BENEFIT_FORMAT("%s: -%,d원"),
+  BENEFIT_MONEY("<총혜택 금액>"),
+  BENEFIT_MONEY_FORMAT("-%,d원"),
+  ORDER_MONEY_AFTER_DISCOUNT("<할인 후 예상 결제 금액>"),
+  ORDER_MONEY_AFTER_DISCOUNT_FORMAT("%,d원"),
+  EVENT_BADGE("<12월 이벤트 배지>");
 
   private final String message;
 
