d33ee2612a69767fe39d791038e101203b362161
KimDongEon <gold3987@naver.com>
2023-11-14 14:30:49 +0900

    feat(Order) : 디저트 메뉴 개수 집계 구현 - countDessertMenu()

--- src/main/java/christmas/model/Order.java
+++ src/main/java/christmas/model/Order.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import christmas.constant.menu.Menu;
+import christmas.constant.menu.MenuCategory;
 import christmas.validator.Validate;
 
 import java.util.HashMap;
@@ -14,6 +15,7 @@ public class Order {
   private final int date;
   private final Map<Menu, Integer> order;
 
+  // 생성자
   public Order(int date, Map<String, Integer> inputOrder) {
     this.date = date;
     Map<Menu, Integer> order = convertOrder(inputOrder);
@@ -22,6 +24,7 @@ public class Order {
     this.order = order;
   }
 
+  // String에서 Menu 변환
   public Map<Menu, Integer> convertOrder(Map<String, Integer> inputOrder) {
     Map<Menu, Integer> order = new HashMap<>();
     for(String name : inputOrder.keySet()) {
@@ -31,6 +34,18 @@ public class Order {
     return order;
   }
 
+
+
+  // 디저트 메뉴 개수 집계
+  public int countDessertMenu() {
+    int count = 0;
+    for(Menu menu : order.keySet()) {
+      if(menu.getCategory().equals(MenuCategory.DESSERT)) {
+        count += 1;
+      }
+    }
+    return count;
+  }
 
 
 
