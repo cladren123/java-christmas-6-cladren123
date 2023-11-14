da518bdb4b6ebf02d4e8ad9b6e5743d1ba13da0c
KimDongEon <gold3987@naver.com>
2023-11-14 21:45:33 +0900

    feat(OutputView) : 인트로 문구 출력 구현

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,9 +1,20 @@
 package christmas.view;
 
 
+import christmas.constant.message.OutputMessage;
+
 /**
  * 출력 화면 생성
  */
 public class OutputView {
+
+  // 인트로 문구 출력
+  public void intro() {
+    System.out.println(OutputMessage.INTRO);
+  }
+
+  
+
+
   
 }
