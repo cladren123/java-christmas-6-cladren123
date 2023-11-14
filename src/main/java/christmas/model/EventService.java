a83df4edb797a8ebc87e407b7dc0d5d8c43ac809
KimDongEon <gold3987@naver.com>
2023-11-14 20:51:12 +0900

    feat(EventService) : 할인 전 총 주문 금액, 증정 메뉴 구현

--- src/main/java/christmas/model/EventService.java
+++ src/main/java/christmas/model/EventService.java
@@ -1,6 +1,8 @@
 package christmas.model;
 
 
+import christmas.constant.menu.Menu;
+import christmas.constant.message.OutputMessage;
 import org.mockito.internal.matchers.Or;
 
 import java.util.Map;
@@ -17,6 +19,20 @@ public class EventService {
     this.order = new Order(date, order);
     this.event = new Event(this.order);
   }
+
+  // 할인 전 총 주문 금액
+  public int orderMoney() {
+    return order.calculateOrderMoney();
+  }
+
+  // 증정 메뉴
+  public String giveaway() {
+    if(order.giveawayEvent() == Menu.CHAMPAGNE.getPrice()) {
+      return OutputMessage.GIVEAWAY_MENU.getMessage();
+    }
+    return OutputMessage.NOTHING.getMessage();
+  }
+
 
 
 
