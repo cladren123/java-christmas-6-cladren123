bab28dc9551713340ffd57aa954ea4644abd21f0
KimDongEon <gold3987@naver.com>
2023-11-14 17:15:51 +0900

    feat(Event) : 특별 할인 함수 구현 - specialDiscount()

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import christmas.constant.number.Number;
+import christmas.constant.number.SpecialDay;
 
 /**
  * 이벤트 로직 처리 (할인, 증정, 이벤트 배지)
@@ -11,7 +12,7 @@ public class Event {
 
   // 크리스마스 디데이 할인 금액
   public int christmasDdayDiscount(int date) {
-    if(date > Number.CHRISTMAS_D_DAY_LIMIT.getNumber()) return 0;
+    if(date > Number.CHRISTMAS_D_DAY_LIMIT.getNumber()) return Number.NOTHING.getNumber();
     return Number.CHRISTMAS_D_DAY_START.getNumber() + Number.CHRISTMAS_D_DAY_DAY.getNumber() * (date-1);
   }
 
@@ -23,6 +24,16 @@ public class Event {
   // 주말 할인 금액 계산 - 메인 메뉴 할인
   public int weekendDiscount(Order order) {
     return order.countMainMenu() * Number.WEEKEND_DISCOUNT.getNumber();
+  }
+
+  // 특별 할인
+  public int specialDiscount(int date) {
+    for(SpecialDay specialDay : SpecialDay.values()) {
+      if (specialDay.getDay() == date) {
+        return Number.SPECIAL_DISCOUNT.getNumber();
+      }
+    }
+    return Number.NOTHING.getNumber();
   }
 
 
