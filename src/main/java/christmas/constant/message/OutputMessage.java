cd350860c56911f728ff0db6c4d70e2ab1a2e5ae
KimDongEon <gold3987@naver.com>
2023-11-14 20:47:13 +0900

    feat(OutputMessage) : 증정 메뉴 내용 추가

--- src/main/java/christmas/constant/message/OutputMessage.java
+++ src/main/java/christmas/constant/message/OutputMessage.java
@@ -6,9 +6,11 @@ package christmas.constant.message;
 public enum OutputMessage {
 
   INTRO("에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n"),
+  NOTHING("없음\n"),
   ORDER_MENU("<주문 메뉴>\n"),
   ORDER_MONEY("<할인 전 총주문 금액>\n"),
   GIVEAWAY_MENU("<증정 메뉴>\n"),
+  GIVEAWAY_MENU_TRUE("샴페인 1개\n"),
   BENEFIT("<혜택 내역>\n"),
   BENEFIT_MONEY("<총혜택 금액>\n"),
   ORDER_MONEY_AFTER_DISCOUNT("<할인 후 예상 결제 금액>\n"),
