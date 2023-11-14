c53319e725bd2d9d5d8b74870ae418d4d8d5a734
KimDongEon <gold3987@naver.com>
2023-11-14 18:37:22 +0900

    feat(Event) : 총 할인 금액 계산 - discountMoney()

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -13,11 +13,9 @@ import java.util.Map;
  */
 public class Event {
 
-  private final Order order;
   private final Map<BenefitMessage, Integer> benefit;
 
   Event(Order order) {
-    this.order = order;
     this.benefit = benefitTotal(order);
   }
 
@@ -30,6 +28,18 @@ public class Event {
     benefit.put(BenefitMessage.GIVEAWAY_EVENT, order.giveawayEvent());
     return benefit;
   }
+
+  // 총 할인 금액 계산
+  public int discountMoney() {
+    int discountMoney = 0;
+    for(BenefitMessage benefitMessage : benefit.keySet()) {
+      discountMoney += benefit.get(benefitMessage);
+    }
+    return discountMoney;
+  }
+
+
+
 
 
 
