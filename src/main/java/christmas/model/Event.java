5c3e96c20a7e9eb7fca03f19a5160f172a508b9d
KimDongEon <gold3987@naver.com>
2023-11-14 15:50:20 +0900

    feat(Event) : 크리스마스 디데이 할인 금액 계산 함수 구현 - christmasDdayDiscount()

--- src/main/java/christmas/model/Event.java
+++ src/main/java/christmas/model/Event.java
@@ -1,7 +1,6 @@
 package christmas.model;
 
-import java.util.HashMap;
-import java.util.Map;
+import christmas.constant.number.Number;
 
 /**
  * 이벤트 로직 처리 (할인, 증정, 이벤트 배지)
@@ -12,6 +11,13 @@ public class Event {
 
   public Event(Order order) {
     this.order = order;
+  }
+
+
+  // 크리스마스 디데이 할인 금액
+  public int christmasDdayDiscount(int date) {
+    if(date > Number.CHRISTMAS_D_DAY_LIMIT.getNumber()) return 0;
+    return Number.CHRISTMAS_D_DAY_START.getNumber() + Number.CHRISTMAS_D_DAY_DAY.getNumber() * (date-1);
   }
 
 
