990deab6bb30d2f773a783fba60ec89200337291
KimDongEon <gold3987@naver.com>
2023-11-15 16:27:10 +0900

    fix(OutputView) : static으로 변경

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -12,13 +12,13 @@ import java.util.Map;
 public class OutputView {
 
   // 인트로 문구 출력
-  public void printIntro(int date) {
+  public static void printIntro(int date) {
     System.out.println(String.format(OutputMessage.INTRO.getMessage(),date));
     System.out.println();
   }
 
   // 주문한 메뉴와 개수 출력
-  public void printMenu(Map<String, Integer> orderMenu) {
+  public static void printMenu(Map<String, Integer> orderMenu) {
     System.out.println(OutputMessage.ORDER_MENU.getMessage());
     for (String name : orderMenu.keySet()) {
       System.out.println(String.format(OutputMessage.ORDER_MENU_FORMAT.getMessage(), name, orderMenu.get(name)));
@@ -27,21 +27,21 @@ public class OutputView {
   }
   
   // 할인 전 총 주문 금액 출력
-  public void printOrderMoney(int orderMoney) {
+  public static void printOrderMoney(int orderMoney) {
     System.out.println(OutputMessage.ORDER_MONEY.getMessage());
     System.out.println(String.format(OutputMessage.ORDER_MONEY_FORMAT.getMessage(), orderMoney));
     System.out.println();
   }
 
   // 증정 메뉴 출력
-  public void printGiveaway(String message) {
+  public static void printGiveaway(String message) {
     System.out.println(OutputMessage.GIVEAWAY_MENU.getMessage());
     System.out.println(message);
     System.out.println();
   }
 
   // 혜택 내역 출력
-  public void printBenefit(Map<BenefitMessage, Integer> benefit) {
+  public static void printBenefit(Map<BenefitMessage, Integer> benefit) {
     System.out.println(OutputMessage.BENEFIT.getMessage());
     if(benefit.size() == 0) {
       System.out.println(OutputMessage.NOTHING.getMessage());
@@ -55,21 +55,21 @@ public class OutputView {
   }
 
   // 총혜택 금액 출력
-  public void printBenefitMoney(int benefitMoney) {
+  public static void printBenefitMoney(int benefitMoney) {
     System.out.println(OutputMessage.BENEFIT_MONEY.getMessage());
     System.out.println(String.format(OutputMessage.BENEFIT_MONEY_FORMAT.getMessage(), benefitMoney * -1));
     System.out.println();
   }
 
   // 할인 후 예상 결제 금액 출력
-  public void printOrderMoneyAfterDiscount(int orderMoneyAfterBenefit) {
+  public static void printOrderMoneyAfterDiscount(int orderMoneyAfterBenefit) {
     System.out.println(OutputMessage.ORDER_MONEY_AFTER_DISCOUNT.getMessage());
     System.out.println(String.format(OutputMessage.ORDER_MONEY_AFTER_DISCOUNT_FORMAT.getMessage(), orderMoneyAfterBenefit));
     System.out.println();
   }
 
   // 12월 이벤트 배지 출력
-  public void printEventBadge(String badge) {
+  public static void printEventBadge(String badge) {
     System.out.println(OutputMessage.EVENT_BADGE.getMessage());
     System.out.println(badge);
   }
