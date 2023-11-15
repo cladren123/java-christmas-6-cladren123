f0af7b64ad8d8578cb1acf8142ffdece0369c7c7
KimDongEon <gold3987@naver.com>
2023-11-15 16:08:29 +0900

    feat(ConverterTest) : order로 혜택 내역 변환 테스트 코드 구현

--- src/test/java/christmas/model/ConverterTest.java
+++ src/test/java/christmas/model/ConverterTest.java
@@ -1,6 +1,7 @@
 package christmas.model;
 
 import christmas.constant.menu.Menu;
+import christmas.constant.message.BenefitMessage;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +28,32 @@ class ConverterTest {
     Map<Menu, Integer> expect = new LinkedHashMap<>(Map.of(
             Menu.MUSHROOM_SOUP, 1,
             Menu.ZERO_COLA, 2
+    ));
+    assertEquals(result, expect);
+  }
+
+  @DisplayName("order에서 혜택 내역 변환")
+  @Test
+  void benefitTotal() {
+    // given
+    int date = 10;
+    Map<Menu, Integer> inputOrder = new LinkedHashMap<>(Map.of(
+            Menu.T_BONE_STEAK, 3,
+            Menu.BBQ_RIBS, 1,
+            Menu.ZERO_COLA, 2,
+            Menu.ICE_CREAM, 1
+    ));
+    Order order = new Order(date, inputOrder);
+
+    // when
+    Map<BenefitMessage, Integer> result = Converter.benefitTotal(order);
+
+    // then
+    Map<BenefitMessage, Integer> expect = new LinkedHashMap<>(Map.of(
+            BenefitMessage.CHRISTMAS_D_DAY, 1900,
+            BenefitMessage.WEEKDAY_DISCOUNT, 2023,
+            BenefitMessage.SPECIAL_DISCOUNT, 1000,
+            BenefitMessage.GIVEAWAY_EVENT, 25000
     ));
     assertEquals(result, expect);
   }
