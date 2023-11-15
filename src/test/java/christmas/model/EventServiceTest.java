3a2f18dec7fe8d1798073f35f50d6ee948cdcf96
KimDongEon <gold3987@naver.com>
2023-11-15 16:36:21 +0900

    feat(EventServiceTest) : 할인 후 예상 결제 금액 테스트 코드 구현

--- src/test/java/christmas/model/EventServiceTest.java
+++ src/test/java/christmas/model/EventServiceTest.java
@@ -16,9 +16,21 @@ class EventServiceTest {
   @Test
   void orderMoneyAfterBenefit() {
     // given
-    int date = 3;
+    int date = 1;
+    Map<Menu, Integer> inputOrder = new LinkedHashMap<>(Map.of(
+            Menu.BBQ_RIBS, 1,
+            Menu.ICE_CREAM, 2
+    ));
+    Order order = new Order(date, inputOrder);
+    Event event = new Event(Converter.benefitTotal(order));
+    EventService eventService = new EventService(order, event);
 
+    // when
+    int result = eventService.orderMoneyAfterBenefit();
 
+    // then
+    int expect = (54000 + 5000 * 2) - (1000 + 2023);
+    assertEquals(result, expect);
   }
 
 }
