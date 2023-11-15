fde367f2b1d1c043f6dbefb7048b074fbf322352
KimDongEon <gold3987@naver.com>
2023-11-15 15:26:55 +0900

    feat(EventTest) : 배지 부여 테스트 코드 구현

--- src/test/java/christmas/model/EventTest.java
+++ src/test/java/christmas/model/EventTest.java
@@ -52,6 +52,78 @@ class EventTest {
     assertEquals(result, expect);
   }
 
+  @DisplayName("배지 부여_산타")
+  @Test
+  void badgeEvent_santa() {
+    // given
+    Map<BenefitMessage, Integer> benefit = new LinkedHashMap<>(Map.of(
+            BenefitMessage.CHRISTMAS_D_DAY, 1300,
+            BenefitMessage.WEEKDAY_DISCOUNT, 2023,
+            BenefitMessage.GIVEAWAY_EVENT, 25000
+    ));
+    Event event = new Event(benefit);
+
+    // when
+    String result = event.badgeEvent();
+
+    // then
+    String expect = "산타";
+    assertEquals(result, expect);
+  }
+
+  @DisplayName("배지 부여_트리")
+  @Test
+  void badgeEvent_tree() {
+    // given
+    Map<BenefitMessage, Integer> benefit = new LinkedHashMap<>(Map.of(
+            BenefitMessage.CHRISTMAS_D_DAY, 1300,
+            BenefitMessage.WEEKDAY_DISCOUNT, 10115
+    ));
+    Event event = new Event(benefit);
+
+    // when
+    String result = event.badgeEvent();
+
+    // then
+    String expect = "트리";
+    assertEquals(result, expect);
+  }
+
+  @DisplayName("배지 부여_별")
+  @Test
+  void badgeEvent_star() {
+    // given
+    Map<BenefitMessage, Integer> benefit = new LinkedHashMap<>(Map.of(
+            BenefitMessage.CHRISTMAS_D_DAY, 1300,
+            BenefitMessage.WEEKDAY_DISCOUNT, 4046
+    ));
+    Event event = new Event(benefit);
+
+    // when
+    String result = event.badgeEvent();
+
+    // then
+    String expect = "별";
+    assertEquals(result, expect);
+  }
+
+  @DisplayName("배지 부여_없음")
+  @Test
+  void badgeEvent_nothing() {
+    // given
+    Map<BenefitMessage, Integer> benefit = new LinkedHashMap<>(Map.of(
+            BenefitMessage.CHRISTMAS_D_DAY, 1300
+    ));
+    Event event = new Event(benefit);
+
+    // when
+    String result = event.badgeEvent();
+
+    // then
+    String expect = "없음";
+    assertEquals(result, expect);
+  }
+
 
 
 }
